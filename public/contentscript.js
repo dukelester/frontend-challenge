6df5f68849361d767f4149e7ab72f8b829083799
dukelester <dukelester4@gmail.com>
2023-01-02 15:18:39 +0300

    Implementation of the DOM Manipulations
    In this file a new element is created, and added to the DOM. An event listener is added to the element for a click event and data being fetched from github and display the paragraph for the description of the first 3 repositories on climate change.

--- public/contentscript.js
+++ public/contentscript.js
@@ -1,9 +1,47 @@
 console.log('Contentscript injected');
+let divElement = document.createElement('div');
+let budgetElement = document.querySelector('.makeStyles-btb-27');
+let budgetValue = budgetElement.textContent;
+divElement.innerText = `Budget-to-Beat: ${budgetValue}`;
+divElement.classList.add('budget');
+const parentElement = document.querySelector('section');
+parentElement.prepend(divElement);
 
-// let element = ...;
+divElement.addEventListener('click', () => {
+  const modal = document.createElement('div');
+  modal.classList.add('modal');
+  modal.innerHTML = `
+    <div class="modal-content">
+      <p>Climate change is the long-term alteration in Earth's climate and weather
+      patterns caused by human activity, such as the burning of fossil fuels and
+      deforestation, as well as natural factors such as changes in the sun's intensity.
+      </p>
+      <p>The effects of climate change include rising sea levels, more frequent and
+      severe weather events, and changes in temperature and precipitation patterns,
+      which can have a range of impacts on ecosystems and human communities.
+      </p>
+      <button class="close-button">Close</button>
+    </div>
+    `;
 
-// ...
+  parentElement.prepend(modal);
+  let closeButton = document.querySelector('.close-button');
+  closeButton.addEventListener('click', () => {
+    modal.remove();
+  });
+});
 
-// const parentElement = document.querySelector(
-//   '#root > div > div > div.makeStyles-mainPanel-3 > div.makeStyles-scrollbars-5 > div:nth-child(1) > div > section > div:nth-child(2) > h2',
-// );
+function createParagraph() {
+  const queryTitle = 'climate change';
+  const apiURL = `https://api.github.com/search/repositories?q=${queryTitle}`;
+  fetch(apiURL)
+    .then((response) => response.json())
+    .then((data) => {
+      const repositories = data.items.slice(0, 3);
+      const paragraph = document.createElement('p');
+      paragraph.innerHTML = repositories.map((repository) => `<b>${repository.description}`).join('<br>');
+      parentElement.append(paragraph);
+    });
+}
+
+createParagraph();
