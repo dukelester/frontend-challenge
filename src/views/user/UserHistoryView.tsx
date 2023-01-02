28470e02c252bcb98187af998b09acd19f5ef054
dukelester <dukelester4@gmail.com>
2023-01-02 15:22:07 +0300

    Adde the table to the Dashboard.
    This is a table where the user will be able to view all the history about the places and time He/she visisted. The data is obtained from dummy data.

--- src/views/user/UserHistoryView.tsx
+++ src/views/user/UserHistoryView.tsx
@@ -1,5 +1,21 @@
 import { ReactElement } from 'react';
 
+import { UserHistory } from './../../components/history/UserHistory';
+
+const dummyData = [
+  {
+    date: '2022-01-01',
+    location: 'San Francisco',
+    distance: 10,
+  },
+  {
+    date: '2022-01-02',
+    location: 'Los Angeles',
+    distance: 20,
+  },
+  // ...
+];
+
 export const UserHistoryView = (): ReactElement => {
-  return <div>{'I could be a table'}</div>;
+  return <UserHistory trips={dummyData} />;
 };
